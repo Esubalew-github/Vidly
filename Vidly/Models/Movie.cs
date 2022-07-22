77be06e337484f734fff80a6352891dd6d7b0f47
Esuablew <nattiy535@gmail.com>
2022-07-22 20:04:54 +0200

    Solution - Section 5 Exercise - Add validation to movie form.

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -13,14 +13,18 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Genre")]
         [Required]
         public Genre Genre { get; set; }
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
 }
