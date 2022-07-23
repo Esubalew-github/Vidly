726ba5c111e7c328c2586036df7771c0b0592146
Esuablew <nattiy535@gmail.com>
2022-07-23 20:20:21 +0200

    Solution - Section 6 Exercise - Build an API for movies.

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -19,10 +19,18 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-        //GET /api/customers
+        /*//GET /api/customers
         public IEnumerable<CustomerDto> GetCustomers()
         {
             return _context.Customers.ToList().Select(Mapper.Map<Customer,CustomerDto>);
+        }*/
+
+        // GET /api/customers
+        public IHttpActionResult GetCustomers()
+        {
+            var customerDtos = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customerDtos);
         }
 
         /*// GET /api/customers/1
@@ -60,8 +68,8 @@ namespace Vidly.Controllers.Api
 
             return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
         }
-        
-        // PUT /api/customers/1
+
+        /*// PUT /api/customers/1
         [HttpPut]
         public void UpdateCustomer(int id, CustomerDto customerDto)
         {
@@ -76,9 +84,28 @@ namespace Vidly.Controllers.Api
 
             _context.SaveChanges();
 
+        }*/
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            Mapper.Map(customerDto, customerInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
         }
-       
-        // DELETE /api/customers/1
+
+        /*// DELETE /api/customers/1
         [HttpDelete]
         public void DeleteCustomer(int id)
         {
@@ -87,6 +114,21 @@ namespace Vidly.Controllers.Api
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             _context.Customers.Remove(customerInDb);
             _context.SaveChanges();
+        }*/
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
